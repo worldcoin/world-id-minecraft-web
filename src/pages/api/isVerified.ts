da1d86245843fdeb375792481209e13cd3ffe640
0xPenryn <penryn@pm.me>
2023-12-11 12:52:52 +0100

    feat: idkit v1 & world id lite

--- src/pages/api/isVerified.ts
+++ src/pages/api/isVerified.ts
@@ -11,18 +11,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("url: ", req.url)
   const uuid = req.query.id as string;
-  console.log("uuid: ", uuid)
   const client = await createClient({
     url: process.env.REDIS_URL, 
     socket: { tls: true }
   })
     .on('error', err => console.log('Redis Client Error', err))
     .connect();
-  const nullifier_hash = await client.get(uuid);
-  if (nullifier_hash) {
-    res.status(200).send({ code: "success", detail: "This credential has been verified!" });
+  const credential = await client.get(uuid);
+  if (credential == "orb") {
+    res.status(200).send("orb");
+  } else if (credential == "device") {
+    res.status(200).send("lite");
   } else {
     res.status(404).send({ code: "not_found", detail: "This credential has not been verified!" });
   }
