2fe48ccb2e702b33b19a84bf4f8570da0e261d45
Miguel Piedrafita <github@miguel.build>
2023-12-13 15:12:51 +0000

    Redirect base url to app store

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      { source: '/', permanent: true, destination: 'https://worldcoin.org/apps/minecraft' },
+    ]
+  },
 }
 
 module.exports = nextConfig
